9ef97aca47df4d826d648019095455b03a36e87f
Thomas Nadal-Elizalde <tnelizalde@iutbayonne.univ-pau.fr>
2020-03-10 16:44:31 +0100

    TP5 - Ajout formulaire login et accès connexion et deconnexion depuis la vue

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -63,9 +63,9 @@ class User implements UserInterface
 		return $this->roles;
         // $roles = $this->roles;
         
-        // $roles[] = 'ROLE_USER'; // guarantee every user at least has ROLE_USER
+        $roles[] = 'ROLE_USER'; // guarantee every user at least has ROLE_USER
 
-        // return array_unique($roles);
+        return array_unique($roles);
     }
 
     public function setRoles(array $roles): self
