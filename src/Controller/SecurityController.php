d7f525abf9802a1555328465c1beb70bb57f8836
Thomas Nadal-Elizalde <tnelizalde@iutbayonne.univ-pau.fr>
2020-03-21 11:10:30 +0100

    Finalisation TP5 - ajout de l'inscription utilisateur

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -2,10 +2,16 @@
 
 namespace App\Controller;
 
+use App\Form\UserType;
+
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
+use Doctrine\Common\Persistence\ObjectManager;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+use App\Entity\User;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 class SecurityController extends AbstractController
 {
@@ -33,4 +39,39 @@ class SecurityController extends AbstractController
     {
         throw new \Exception('This method can be blank - it will be intercepted by the logout key on your firewall');
     }
+
+
+
+    /**
+     * @Route("/inscription", name="app_inscription")
+     */
+	public function inscription(Request $request, ObjectManager $manager, UserPasswordEncoderInterface $encoder){
+		//Création de l'objet utilisateur
+		$utilisateur = new User();
+		
+		//Création du formulaire de création de l'utilisateur
+		$formulaireCreationUtilisateur = $this -> createForm(UserType::class, $utilisateur);
+		
+		$formulaireCreationUtilisateur->handleRequest($request);
+		
+		if($formulaireCreationUtilisateur->isSubmitted() && $formulaireCreationUtilisateur->isValid()){
+            //attribuer rôle à utilisateur
+            $utilisateur->setRoles(['ROLE_USER']);
+
+            //encoder mdp utilisateur
+            $encodagePassword = $encoder->encodePassword($utilisateur, $utilisateur->getPassword());
+            $utilisateur->setPassword($encodagePassword);
+
+			//Enregistrer utilisateur en BD
+			$manager->persist($utilisateur);
+			$manager->flush();
+			
+			//rediriger utilisateur vers la page d'accueil
+			return $this->redirectToRoute("app_login");
+		}
+		
+		//envoi du formulaire à la vue
+		return $this->render('security/inscription.html.twig',['vueFormulaire' => $formulaireCreationUtilisateur->createView()]);
+	}
+
 }
